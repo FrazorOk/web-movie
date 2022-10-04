c9f1af153a32d340e60a52fbece5372298f43e3e
FrazorOk <Dimoniw1996@gmail.com>
2022-10-04 16:19:29 +0300

    hash router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<HashRouter basename="frazorok.github.io/web-movie">
+	<HashRouter>
 		<Provider store={store}>
 			<App />
 		</Provider>
