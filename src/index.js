baf1847eac21c18399b9301f7ecf0cdc9b4d1512
FrazorOk <Dimoniw1996@gmail.com>
2022-10-04 16:17:49 +0300

    Configure React app for deployment to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<BrowserRouter>
+	<HashRouter basename="frazorok.github.io/web-movie">
 		<Provider store={store}>
 			<App />
 		</Provider>
-	</BrowserRouter>
+	</HashRouter>
 );
